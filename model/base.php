a65c9dbd41fc974471da99aaaa0dca6189712bf1
zhao.binyan <itbudaoweng@gmail.com>
2014-04-27 04:01:30 +0800

    format code

--- model/base.php
+++ model/base.php
@@ -15,8 +15,8 @@ class Model_Base {
         if (isset($GLOBALS[$db_name])) {
             $db_config = $GLOBALS[$db_name];
             $handle = new Library_Mysqli($db_config['host'], $db_config['username'], $db_config['password'], $db_config['database'], $db_config['port']);
-            if ($handle->errno) {
-                trigger_error('mysql_error:'.$this->handle->errno.$this->handle->error);
+            if ($handle->connect_errno) {
+                trigger_error('mysql_connect_error:'.$handle->connect_errno.$handle->connect_error);
             }
 
             $handle->set_charset('utf8');
