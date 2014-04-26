54f932275d29436502ccf854b058338fad55f93a
zhao.binyan <itbudaoweng@gmail.com>
2014-04-27 03:43:36 +0800

    format code

--- model/base.php
+++ model/base.php
@@ -7,8 +7,6 @@
  */
 class Model_Base {
 
-    public $handle;
-
     public function __construct() {
 
     }
@@ -16,15 +14,14 @@ class Model_Base {
     public function getMysql($db_name) {
         if (isset($GLOBALS[$db_name])) {
             $db_config = $GLOBALS[$db_name];
-            $this->handle = new Library_Mysqli($db_config['host'], $db_config['username'], $db_config['password'], $db_config['database'], $db_config['port']);
-            if ($this->handle->errno) {
+            $handle = new Library_Mysqli($db_config['host'], $db_config['username'], $db_config['password'], $db_config['database'], $db_config['port']);
+            if ($handle->errno) {
                 trigger_error('mysql_error:'.$this->handle->errno.$this->handle->error);
             }
 
-//            $this->handle->select_db($db_config['database']);
-            $this->handle->set_charset('utf8');
+            $handle->set_charset('utf8');
 
-            return $this->handle;
+            return $handle;
         } else {
             trigger_error('could not connect to database ' . $db_name);
         }
