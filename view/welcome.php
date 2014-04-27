f62ec84e82ab20dc7a0ac5b85a154a76399af278
zhao.binyan <itbudaoweng@gmail.com>
2014-04-27 19:01:13 +0800

    format code

--- view/welcome.php
+++ view/welcome.php
@@ -5,7 +5,6 @@
     <meta http-equiv="content-type" content="text/html;charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="<?php echo $static_base_url ?>/static/css/bootstrap.min.css"/>
-<!--    <link rel="stylesheet" href="--><?php //echo $static_base_url ?><!--/static/css/bootstrap-theme.min.css"/>-->
 </head>
 <body>
 
